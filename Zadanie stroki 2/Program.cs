3bbd025196d62f41c3a19d2cac43ac594f36c9e2
NikolaiKruchkov <47074017+NikolaiKruchkov@users.noreply.github.com>
2021-11-22 16:55:01 +0300

    Add files via upload

---
+++ Zadanie stroki 2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Zadanie_stroki_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение, слова должны быть разделены пробелом, знаки препинания не использовать, затем нажмите Enter");
+            string predlogenie = Console.ReadLine();
+            predlogenie.ToLower();
+            string bez_probelov = predlogenie.Replace(" ", "");            
+            string[] massivstr = bez_probelov.Split();
+            int a = massivstr.Length;
+
+            foreach (string i in massivstr)
+            {
+                if (i == massivstr[a - 1])
+                {
+                    a -= 1;
+                }
+            }                  
+                
+            if  (a==0)
+            {
+                Console.WriteLine("Предложение является палиндромом");
+
+            }
+            else
+            {
+                Console.WriteLine("Предложение не является палиндромом");
+            }
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Zadanie stroki/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Zadanie_stroki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение, слова должны быть разделены пробелом, знаки препинания не использовать, затем нажмите Enter");
+            string predlogenie = Console.ReadLine();
+            string[] massivstr = predlogenie.Split();
+            string max = massivstr[0];
+            int a = 0;
+            int b = 0;
+
+            foreach (string i in massivstr)
+            {
+                a = max.Length;
+
+                b = i.Length;
+
+                if (a < b)
+                {
+                    max = i;
+                }
+
+            }
+
+
+            Console.WriteLine("Самым длинным в предложении является слово \"{0}\"", max);
+            Console.WriteLine("Спасибо, что использовали нашу пргорамму, для выхода нажмите любую клавишу");
+
+            Console.ReadKey();
+        }
+
+    }
+}
